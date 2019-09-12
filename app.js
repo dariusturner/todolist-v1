3c50a2f94aedc2fa5188f1d18490b875a73a39a4
Darius Turner <dariusot731@gmail.com>
2019-09-12 08:49:26 -0400

    Set up app.js

---
+++ app.js
@@ -0,0 +1,15 @@
+//jshint esversion:6
+
+const express = require ("express");
+const bodyParser = require("body-parser");
+
+const app = express();
+const port = 3000;
+
+app.get("/", function(req, res){
+  res.sendFile(__dirname + "index.html");
+});
+
+app.listen(port, function(){
+  console.log("Server started on port " + port);
+});
