90e497ab1c974a681ac409861306f4ec4a55211b
Darius Turner <dariusot731@gmail.com>
2019-09-12 08:56:02 -0400

    Set up index.html and fixed app.js issue not showing the index.html - Complete

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const app = express();
 const port = 3000;
 
 app.get("/", function(req, res){
-  res.sendFile(__dirname + "index.html");
+  res.sendFile(__dirname + "/index.html");
 });
 
 app.listen(port, function(){
